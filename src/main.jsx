f965b82b72c198bdea6301d79b143558de118a84
colehanekom <Coleh304@gmail.com>
2023-08-09 14:46:25 +0000

    2nd commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+import 'tailwindcss/tailwind.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
