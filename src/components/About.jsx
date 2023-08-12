923f299755c84751c34d8d6b866b6ee9916b0707
colehanekom <Coleh304@gmail.com>
2023-08-12 17:39:31 +0000

    updated Logo name to Image. Updated paragraph text size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/aboutimg.jpg'
+import Image from '../assets/aboutimg.jpg'
 
 const About = () => {
   return (
@@ -13,11 +13,11 @@ const About = () => {
             </div>
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                 <div>
-                  <img src={Logo} alt="Logo Image" style={{width:'350px'}}/>
+                  <img src={Image} alt="Image" style={{width:'350px'}}/>
                 </div>
 
             <div>
-              <p> As a proud graduate from CPUT (Cape Peninsula University of Technology), I am a passionate and versatile coder proficient in multiple programming languages. With a solid foundation in Java, Python, HTML, CSS, and JavaScript, I bring a diverse skill set to the table. My love for coding stems from its limitless possibilities and the ability to bring ideas to life through technology. Throughout my academic journey, I have tackled various projects, honing my problem-solving skills and developing a keen eye for detail. I thrive in dynamic environments, always eager to learn and adapt to new technologies and industry trends. With a dedication to clean and efficient code, I am committed to delivering high-quality solutions that exceed expectations. I am excited about the ever-evolving world of technology and eager to contribute my skills and passion to exciting projects.
+              <p className='text-[14x]'> As a proud graduate from CPUT (Cape Peninsula University of Technology), I am a passionate and versatile coder proficient in multiple programming languages. With a solid foundation in Java, Python, HTML, CSS, and JavaScript, I bring a diverse skill set to the table. My love for coding stems from its limitless possibilities and the ability to bring ideas to life through technology. Throughout my academic journey, I have tackled various projects, honing my problem-solving skills and developing a keen eye for detail. I thrive in dynamic environments, always eager to learn and adapt to new technologies and industry trends. With a dedication to clean and efficient code, I am committed to delivering high-quality solutions that exceed expectations. I am excited about the ever-evolving world of technology and eager to contribute my skills and passion to exciting projects.
               </p>
             </div>
           </div>
