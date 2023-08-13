c31cfa9000d2048e38e50136623265b55c11bf87
colehanekom <Coleh304@gmail.com>
2023-08-13 17:12:32 +0000

    Imported Footer to App.jsx. Added code for Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div name='footer' className='w-full h-9 bg-[#111111] text-white p-4'>
+        <p>Copyright  2023 by Cole Hanekom All Rights Reserved.</p>
+    </div>
+   
+  )
+}
+
+export default Footer
