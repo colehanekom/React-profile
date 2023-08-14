1391d27a836d03d527ef1b04bf7488debeba7d45
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-08-14 10:57:16 +0200

    Added code to link navbar items to section when in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,12 +40,22 @@ const Navbar = () => {
         </div>
 
         {/*Mobile menu*/}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-zinc-950 flex flex-col justify-center items-center'}>
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">About Me</li>
-          <li className="py-6 text-4xl">Skills</li>
-          <li className="py-6 text-4xl">Projects</li>
-          <li className="py-6 text-4xl">Contact Me</li>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#111111] flex flex-col justify-center items-center'}>
+          <li className="py-4 text-4xl"><Link onClick={handleClick} activeClass="active" to="home"  smooth={true}  duration={500}>
+          Home
+        </Link></li>
+          <li className="py-4 text-4xl"><Link onClick={handleClick} to="about"  smooth={true}  duration={500} >
+          About Me
+        </Link></li>
+          <li className="py-4 text-4xl"><Link onClick={handleClick} to="skills"  smooth={true}  duration={500} >
+          Skills
+        </Link></li>
+          <li className="py-4 text-4xl"><Link onClick={handleClick} to="projects"  smooth={true}  duration={500} >
+          Projects
+        </Link></li>
+          <li className="py-4 text-4xl"><Link onClick={handleClick} to="contact"  smooth={true}  duration={500} >
+          Contact Me
+        </Link></li>
         </ul>
     </div>
   )
