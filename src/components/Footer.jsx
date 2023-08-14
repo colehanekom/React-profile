48899a2eab5766cfba3a634ee3f4201e628bad85
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-08-14 14:59:29 +0200

    Added boxicon for copyright text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div name='footer' className='w-full h-9 bg-[#111111] text-white p-4'>
-        <p>Copyright  2023 by Cole Hanekom All Rights Reserved.</p>
+        <div></div><p>Copyright <i class='bx bx-copyright'></i> 2023 by Cole Hanekom All Rights Reserved.</p>
     </div>
    
   )
