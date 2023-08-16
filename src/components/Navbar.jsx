76f5f9616113361b9eebb9c331d73c20d96ac8e9
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-08-16 09:40:40 +0200

    Adjusted width of mobile menu navbar. Updated text size of mobile menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,20 +40,20 @@ const Navbar = () => {
         </div>
 
         {/*Mobile menu*/}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#111111] flex flex-col justify-center items-center'}>
-          <li className="py-4 text-4xl"><Link onClick={handleClick} activeClass="active" to="home"  smooth={true}  duration={500}>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-56 bg-[#111111] flex flex-col justify-center items-center'}>
+          <li className="py-4 text-1xl"><Link onClick={handleClick} activeClass="active" to="home"  smooth={true}  duration={500}>
           Home
         </Link></li>
-          <li className="py-4 text-4xl"><Link onClick={handleClick} to="about"  smooth={true}  duration={500} >
+          <li className="py-4 text-1xl"><Link onClick={handleClick} to="about"  smooth={true}  duration={500} >
           About Me
         </Link></li>
-          <li className="py-4 text-4xl"><Link onClick={handleClick} to="skills"  smooth={true}  duration={500} >
+          <li className="py-4 text-1xl"><Link onClick={handleClick} to="skills"  smooth={true}  duration={500} >
           Skills
         </Link></li>
-          <li className="py-4 text-4xl"><Link onClick={handleClick} to="projects"  smooth={true}  duration={500} >
+          <li className="py-4 text-1xl"><Link onClick={handleClick} to="projects"  smooth={true}  duration={500} >
           Projects
         </Link></li>
-          <li className="py-4 text-4xl"><Link onClick={handleClick} to="contact"  smooth={true}  duration={500} >
+          <li className="py-4 text-1xl"><Link onClick={handleClick} to="contact"  smooth={true}  duration={500} >
           Contact Me
         </Link></li>
         </ul>
