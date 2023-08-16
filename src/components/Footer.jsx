936bb3011a36e02bc1537eb23c2c27ccf676d01c
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-08-16 14:52:40 +0200

    Added bottom padding to text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div name='footer' className='w-full h-9 bg-[#111111] text-white p-4'>
+    <div name='footer' className='w-full h-9 bg-[#111111] text-white p-4 pb-7'>
         <div></div><p>Copyright <i class='bx bx-copyright'></i> 2023 by Cole Hanekom All Rights Reserved.</p>
     </div>
    
