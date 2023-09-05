585029d68d4f2e644eb3ffac6046284dd34db9fd
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-09-05 14:32:21 +0200

    Increased size of Contact Me heading. Shifted heading down to be aligned with form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -136,14 +136,10 @@ const [userData, setUserData] = useState(
   return (
     <div name="contact" className="w-full min-h-screen bg-[#111111] flex justify-center items-center p-4">
       <div className="flex flex-col justify-center items-center w-full h-full text-white">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-left pb-4 pt-11 pl-4">
-            <p className="text-4xl font-bold">Contact <span className="text-[#10C623]">Me</span></p>
-          </div>
-        </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div>
-            <p className="text-3xl pb-2">If You Have Any Project In Mind</p>
+            <p className="text-4xl lg:text-7xl font-bold">Contact <span className="text-[#10C623]">Me</span></p>
+            <p className="text-3xl pb-2 pt-4">If You Have Any Project In Mind</p>
             <p>
               I would be delighted to hear from you! Please don't hesitate to reach out using the contact information provided below. Whether you have a question, a potential collaboration in mind, or simply want to connect, I am always open to new opportunities. Feel free to email me or connect with me on LinkedIn. I strive to be responsive and will do my best to get back to you promptly. Let's connect and explore how we can work together to create amazing things in the world of technology. I look forward to hearing from you!
             </p>
