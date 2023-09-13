d2fe834f17b6dce758cbb6610f2b951a59ef7a47
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-09-13 10:42:41 +0200

    Added text colour to key words in paragraph text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,15 +13,15 @@ const About = () => {
               <p className='text-2xl md:text-3xl lg:text-7xl font-bold mb-2'>
                 About <span className='text-[#10C623]'>Me</span>
               </p>
-              <p className='text-sm sm:text-base md:text-lg'>
+               <p className='text-sm sm:text-base md:text-lg'>
                 As a proud graduate from CPUT (Cape Peninsula University of Technology), I am a passionate and versatile
-                coder proficient in multiple programming languages. With a solid foundation in Java, Python, HTML, CSS, and
-                JavaScript, I bring a diverse skill set to the table. My love for coding stems from its limitless
-                possibilities and the ability to bring ideas to life through technology. Throughout my academic journey, I
-                have tackled various projects, honing my problem-solving skills and developing a keen eye for detail. I
-                thrive in dynamic environments, always eager to learn and adapt to new technologies and industry trends.
+                coder proficient in multiple programming languages. With a solid foundation in <span className='text-[#10C623]'>Java, Python, HTML, CSS, and
+                JavaScript</span>, I bring a diverse skill set to the table. My love for coding stems from its <span className='text-[#10C623]'>limitless
+                possibilities</span> and the ability to bring ideas to life through technology. Throughout my academic journey, I
+                have tackled various projects, <span className='text-[#10C623]'>honing my problem-solving skills</span> and developing a keen eye for detail. I
+                thrive in dynamic environments, always <span className='text-[#10C623]'>eager to learn </span>and adapt to new technologies and industry trends.
                 With a dedication to clean and efficient code, I am committed to delivering high-quality solutions that
-                exceed expectations. I am excited about the ever-evolving world of technology and eager to contribute my
+                exceed expectations. I am excited about the ever-evolving world of technology and <span className='text-[#10C623]'>eager to contribute </span>my
                 skills and passion to exciting projects.
               </p>
             </div>
