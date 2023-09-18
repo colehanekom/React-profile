57db600ad2e89da2aa2fcde26e565ac62016c2be
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-09-18 14:01:27 +0200

    Increased text size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 pr-14 flex flex-col justify-center w-full h-full'>
         <div className='pb-14 text-center'>
-          <p className='text-3xl md:text-4xl lg:text-7xl font-bold lg:mb-16'>
+          <p className='text-4xl md:text-4xl lg:text-7xl font-bold lg:mb-16'>
             Latest <span className='text-[#10C623]'>Projects</span>
           </p>
         </div>
